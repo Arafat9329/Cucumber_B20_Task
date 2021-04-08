0a3803c51e2f5549c6d3d919fe1bf78e93478917
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2021-04-08 10:08:16 -0400

    Arpat

--- src/test/java/com/vytrack/page/vytrack/Test1.java
+++ src/test/java/com/vytrack/page/vytrack/Test1.java
@@ -3,7 +3,7 @@ package com.vytrack.page.vytrack;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Toronto");
-
+        System.out.println("Hello Test");
+        System.out.println("no hello");
     }
 }
