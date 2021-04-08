ed39f38c39fc19446b22a29ae556600d580ba4ed
Alimu-A <68973633+Deenen@users.noreply.github.com>
2021-04-08 10:11:16 -0400

    test2

--- src/test/java/com/vytrack/page/vytrack/Test1.java
+++ src/test/java/com/vytrack/page/vytrack/Test1.java
@@ -3,7 +3,8 @@ package com.vytrack.page.vytrack;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Toronto");
+        System.out.println("Hello 111111111111");
+        System.out.println("222222222222");
 
     }
 }
