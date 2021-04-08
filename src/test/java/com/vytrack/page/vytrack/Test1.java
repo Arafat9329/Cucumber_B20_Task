04c895aec0b8a252956a27313cc8a8922a4090ab
UmitUmit <umit.green@gmail.com>
2021-04-08 10:08:33 -0400

    New brach

--- src/test/java/com/vytrack/page/vytrack/Test1.java
+++ src/test/java/com/vytrack/page/vytrack/Test1.java
@@ -3,7 +3,9 @@ package com.vytrack.page.vytrack;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Toronto");
+        System.out.println("Hello Mars");
+        System.out.println("Hello Shireli Shukur Arpat Nurshat");
+
 
     }
 }
